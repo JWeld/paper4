68f4b357d3015549601526e1ac1ed6bfb98bb747
JWeld <JWeld@users.noreply.github.com>
2024-04-22 15:55:39 +0200

    subplot level NMDS plotting

--- VG_analysis2.R
+++ VG_analysis2.R
@@ -18,14 +18,30 @@ ordispe_h <- decostand(ordispe_sub, "hellinger") #hellinger transform to make us
 #Exclude sites with 
 #Ordinations####
 #NMDS####
-#subplots
+#subplots #ordispe_sub
 nmds_sub <- metaMDS(ordispe_sub, distance = "bray", k=2, parallel = 8, try =20, trymax = 25)
+#ordiplot(nmds_sub)
+#stressplot(nmds_sub)
+pl <- ordiplot(nmds_sub, display = "sites", type="none")
+text(pl, "sites", cex=0.5, col="grey60", bg="grey80")
+ordiellipse (pl, groups = ordi_extras$ID_plot,
+             label = T, col = "red")
+
+#Weird outliers for 2016_SE14_0001_24? and 2016_SE14_0001_5?
+check <- rownames_to_column(ordispe_sub, var="ID")
+check2 <- filter(check, ID !="2016_SE14_0001_24")
+check2 <- filter(check2, ID !="2016_SE14_0001_5")
+check2 <- column_to_rownames(check2, "ID")
+
+nmds_sub <- metaMDS(check2, distance = "bray", k=2, parallel = 8, try =20, trymax = 25)
 #ordiplot(nmds_sub)
 #stressplot(nmds_sub)
 pl <- ordiplot(nmds_sub, display = "sites", type="none")
 points(pl, "sites", pch=21, col="grey60", bg="grey80")
 ordiellipse (pl, groups = ordi_extras$ID_plot,
              label = T, col = "red")
+
+
 #plots
 nmds_pl <- metaMDS(ordispe_pl, distance = "bray", k=2, trymax = 100)
 ordiplot(nmds_pl)
@@ -50,6 +66,8 @@ NMDS_sub_sp_scores <- NMDS_sub_scores$species
 NMDS_pl_sites_scores <- NMDS_pl_scores$sites
 NMDS_pl_sites_scores <- NMDS_pl_sites_scores %>% as.data.frame() %>% 
   rownames_to_column(var="ID_fine") %>% distinct()
+NMDS_pl_sites_scores <- left_join(NMDS_pl_sites_scores, extras, by = "ID_fine")
+
 # NMDS_pl_sites_scores <- left_join(NMDS_pl_sites_scores, select(extras, -c(ID_fine2, ID_subplot)), by = "ID_fine") %>% 
 #   distinct() %>%
 #   filter(!ID_site %in% c("AT01", "CZ02", "IT01","IT02",
@@ -67,14 +85,14 @@ ggplot(NMDS_sub_sites_scores, aes(NMDS1, NMDS2, colour = ID_plot)) + geom_point(
 df_labels <- aggregate(cbind(NMDS1, NMDS2) ~ ID_plot, data = NMDS_pl_sites_scores, FUN = mean)
 
 # Create the plot
-ggplot(filter(NMDS_sub_sites_scores, NMDS1 > -10),aes(x = NMDS1, y = NMDS2, group = ID_plot, color = ID_plot)) +
-  #geom_path(linewidth=1.25, arrow=arrow(length= unit(4, "mm"), type = "open")) +  # Connect points within each group
+ggplot(filter(NMDS_sub_sites_scores, NMDS1 > -10),aes(x = NMDS1, y = NMDS2, group = ID_subplot, color = ID_subplot)) +
+  geom_path(linewidth=0.5, arrow=arrow(length= unit(4, "mm"), type = "open")) +  # Connect points within each group
   geom_point() + # Plot points
-  geom_text(data = df_labels, aes(label = ID_plot, x = NMDS1, y = NMDS2), vjust = 0,
-            check_overlap = T) + # Add group labels
+  #geom_text(data = df_labels, aes(label = ID_plot, x = NMDS1, y = NMDS2), vjust = 0,
+   #         check_overlap = T) + # Add group labels
   theme_bw() +  # Optional: Use a minimal theme for the plot
-  theme(legend.position = "none")
-  #labs(color = "Group") # Label the legend
+  theme(legend.position = "none")+
+  labs(color = "Group") # Label the legend
 
 # Create the plot
 ggplot(NMDS_pl_sites_scores, aes(x = NMDS1, y = NMDS2, group = ID_plot, color = ID_plot)) +
