8594604b78bf3fc357a61068842850f67bf887d7
JWeld <JWeld@users.noreply.github.com>
2024-08-14 15:36:12 +0200

    Update 04_GLLVM.R

--- 04_GLLVM.R
+++ 04_GLLVM.R
@@ -15,18 +15,32 @@ sample.s   <- ordispe[!sample, ]
 sample.s <- sample.s[, colSums(sample.s != 0) > 0]
 sample.l <- sample.s[, colSums(sample.l != 0) > 0]
 
-fit_base <- gllvm(sample.s, num.lv = 2, starting.val ="zero", family = "gaussian", gradient.check = T)
+fit_base <- gllvm(sample.s, num.lv = 2, starting.val ="zero", family = "gaussian")
 summary(fit_base)
-plot(fit_base, which = 1:2, var.colors = 1)
-ordiplot(fit_base)
-coefplot(fit_base)
+par(mfrow = c(3, 2), mar = c(4, 4, 2, 1))
+plot(fit_base, var.colors = 1)
+#not looking good! Other family values...
+fit_base <- gllvm(sample.s, num.lv = 2, starting.val ="zero", family = "negative.binomial")
+plot(fit_base, var.colors = 1)
+
+par(mfrow = c(1,1))
+ordiplot(fit_base, biplot = TRUE, ind.spp = 15, xlim = c(-3, 3), ylim = c(-3, 3), 
+         main = "Biplot")
+ordiplot(fit_base, biplot = FALSE, ind.spp = 15, xlim = c(-3, 3), ylim = c(-3, 3), 
+         main = "Ordination plot", predict.region = TRUE)
 
 y <- as.matrix(ordispe)
 X <- as.matrix(ordi_extras)
 # Fit model with environmental covariates
 fit <- gllvm(y, X, formula = ~ NH4M + NO3M, num.lv = 2, starting.val ="zero",
-             family = "gaussian")
-ordiplot(fit)
+             family = "negative.binomial")
+par(mfrow = c(3, 2), mar = c(4, 4, 2, 1))
+plot(fit_base, var.colors = 1)
+par(mfrow = c(1,1))
+ordiplot(fit_base, biplot = TRUE, ind.spp = 15, xlim = c(-3, 3), ylim = c(-3, 3), 
+         main = "Biplot")
+ordiplot(fit_base, biplot = FALSE, ind.spp = 15, xlim = c(-3, 3), ylim = c(-3, 3), 
+         main = "Ordination plot", predict.region = TRUE)
 coefplot(fit)
 
 
